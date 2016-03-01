958b824bf7b9476dd2cbe4f6508a93b16d9e585a
KoutarouYabe <idolm@ster.pw>
2016-03-01 20:19:46 +0900

    sorting the layout

---
+++ app/src/main/java/ws/temp/jadestern/fragment/TimelineFragment.java
@@ -0,0 +1,29 @@
+package ws.temp.jadestern.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import butterknife.ButterKnife;
+import ws.temp.jadestern.R;
+
+public class TimelineFragment extends Fragment {
+    private static final String TAG = TimelineFragment.class.getSimpleName();
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_timeline, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+}
